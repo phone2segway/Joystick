2fb1363b1c8e83a658a90397291e717687a782bb
phone2segway <ingarg@stud.ntnu.no>
2016-03-15 11:45:27 +0100

    Update joystick_view.js

--- js/joystick_view.js
+++ js/joystick_view.js
@@ -73,10 +73,8 @@ JoystickView = Backbone.View.extend({
         this.state = INACTIVE;
         this.x = 0;
         this.y = 0;
-        var xVal = 0;
-        var yVal = 0;
         
-        this.renderSprite();
+            this.renderSprite();
     },
     move: function(evt){
         if(this.state == INACTIVE){
